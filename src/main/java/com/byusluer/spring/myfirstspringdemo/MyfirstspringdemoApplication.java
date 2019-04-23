9c0a539a28faaa34dc904c284e2ae0446329267c
trusluel <lutfi.usluer@sony.com>
2019-04-23 18:41:42 +0300

    First Spring App: Books&Authors

---
+++ src/main/java/com/byusluer/spring/myfirstspringdemo/MyfirstspringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.byusluer.spring.myfirstspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstspringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstspringdemoApplication.class, args);
+	}
+
+}
